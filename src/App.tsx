a5d14b069fac19669c6291d0458386ca166b7c01
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-05 10:14:48 +0200

    Configured eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Hello Karaoke World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

366810c6035b43f2563a04eeec315035898b57dc
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-15 15:22:40 +0200

    Photo visible once it's loaded

--- src/components/SelfiePreview.tsx
+++ src/components/SelfiePreview.tsx
@@ -11,27 +11,32 @@ interface PreviewElementProps {
 export function PreviewElement(props: PreviewElementProps) {
   const { objectURL, removeImage } = props;
 
+  const [display, setDisplay] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState('transparent');
   const [transitioning, startTransition] = useTransition();
 
   const workingCanvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
   return (
-    <div className="selfie-preview" style={{ backgroundColor: transitioning ? 'transparent' : backgroundColor }}>
+    <div className={'selfie-preview'} style={{ backgroundColor: transitioning ? 'transparent' : backgroundColor }}>
       <img
         alt="Uploaded selfie"
         src={objectURL}
+        style={{ opacity: display ? 1 : 0, transition: 'opacity 0.5s' }}
         onLoad={(e) => {
           e.preventDefault();
           const imgEl = e.target as HTMLImageElement;
+          setDisplay(true);
           startTransition(() => {
             setBackgroundColor(() => calculateBackgroundColor(workingCanvasRef.current, imgEl));
           });
         }}
       />
+
       <button
         className="close-button"
         onClick={(e) => {
           e.preventDefault();
+          setDisplay(false);
           removeImage();
         }}
       >

63df7fb04f8672a7116d7a8378ba834dd1e1e3df
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-15 14:45:18 +0200

    Making use of useTransition

---
+++ src/components/SelfiePreview.tsx
@@ -0,0 +1,42 @@
+import { useRef, useState, useTransition } from 'react';
+import { calculateBackgroundColor } from '../utils/color-utils';
+
+import './SelfiePreview.css';
+
+interface PreviewElementProps {
+  objectURL: string;
+  removeImage: () => void;
+}
+
+export function PreviewElement(props: PreviewElementProps) {
+  const { objectURL, removeImage } = props;
+
+  const [backgroundColor, setBackgroundColor] = useState('red');
+  const [transitioning, startTransition] = useTransition();
+
+  const workingCanvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
+  return (
+    <div className="selfie-preview" style={{ backgroundColor: transitioning ? 'red' : backgroundColor }}>
+      <img
+        alt="Uploaded selfie"
+        src={objectURL}
+        onLoad={(e) => {
+          e.preventDefault();
+          const imgEl = e.target as HTMLImageElement;
+          startTransition(() => {
+            setBackgroundColor(() => calculateBackgroundColor(workingCanvasRef.current, imgEl));
+          });
+        }}
+      />
+      <button
+        className="close-button"
+        onClick={(e) => {
+          e.preventDefault();
+          removeImage();
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+}
